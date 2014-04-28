3a91c306a67cb64e8cfe255b92d514113cd62786
Jhn587 <jhn587@gmail.com>
2014-04-27 21:18:19 -0400

    So basic stuff

---
+++ Choice.java
@@ -0,0 +1,22 @@
+public class Choice implements Comparable{
+	//Look up enums for the choice
+	
+	public Choice(){
+	
+	
+	}
+	
+	public int compareTo(){
+	
+	
+	}
+	
+	public boolean equals(Object toCheck){
+	
+	
+	
+	}
+
+
+
+}
