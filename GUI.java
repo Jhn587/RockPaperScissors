baf3443decd57101fb8b6b61b3d2293a037ee409
Jhn587 <jhn587@gmail.com>
2014-04-30 21:43:03 -0400

    Gui work

--- GUI.java
+++ GUI.java
@@ -1,5 +1,36 @@
+import java.swingx.*;
+
 public class GUI {
 
-
+	public GUI(){
+		JFrame mainFrame = new JFrame();
+		JPanel thePanel = new JPanel();
+		JRadioButton rockButton = new JRadioButton("Rock");
+		JRadioButton paperButton = new JRadioButton("Paper");
+		JRadioButton scissorButton = new JRadioButton("Scissors");
+		JRadioButton lizardButton = new JRadioButton("Lizard");
+		JRadioButton spockButton = new JRadioButton("Spock");
+		
+		ButtonGroup choices = new ButtonGroup();
+		choices.add(rockButton);
+		choices.add(paperButton);
+		choices.add(scissorsButton);
+		choices.add(lizardButton);
+		choices.add(spockButton);
+		
+		rockButton.addActionListener(this);
+		paperButton.addActionListener(this);
+		scissorButton.addActionListener(this);
+		lizardButton.addActionListener(this);
+		spockButton.addActionListener(this);
+		
+		JButton submit = new JButton("Submit Choice");
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		//We want to have the picture of whatever changed
+	
+	}
 
 }
