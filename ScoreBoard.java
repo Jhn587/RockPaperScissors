cd1607ecaec9016b1239f0797b596deaafdd826e
Jhn587 <jhn587@gmail.com>
2014-05-06 21:55:37 -0400

    ScoreBoard Work

--- ScoreBoard.java
+++ ScoreBoard.java
@@ -1,5 +1,35 @@
 public class ScoreBoard{
 
-
-
+	private int cpuWins = 0;
+	private int userWins = 0;
+	private int gamesPlayed = 0;
+	
+	public ScoreBoard(){
+	
+	}
+	
+	public int userWins(){
+		this.userWins++;
+		this.gamesPlayed++;
+	}
+	
+	public int cpuWins(){
+		this.cpuWins++;
+		this.gamesPlayed++;
+	}
+	
+	public int getGamesPlayed(){
+		return this.gamesPlayed;
+	
+	}
+	
+	public int getCPUWins(){
+		return this.cpuWins;
+	
+	}
+	
+	public int getUserWins(){
+		return this.userWins;
+	
+	}
 }
